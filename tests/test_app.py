b274c2bb7df36fa04fdc7a486b2e3258f628fc6b
Umesh Motwani <umesh.motwani@slalom.com>
2025-05-21 07:53:23 -0400

    delete test app python

--- tests/test_app.py
+++
@@ -1,7 +0,0 @@
-import pytest
-from src.app import app
-
-def test_get_books(client):
-    response = client.get('/api/books')
-    assert response.status_code == 200
-    assert len(response.json['books']) == 3
